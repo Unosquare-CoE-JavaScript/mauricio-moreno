2c1ce2a9b7ad1dd6d85ea3ff6f536eab60d2b277
MauFrontend <104452684+MauFrontend@users.noreply.github.com>
2022-05-06 12:54:05 -0500

    Comments to functors file

--- 02-hardcore-functional-javascript/code/Functors.js
+++ 02-hardcore-functional-javascript/code/Functors.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// The right functor will wrap again the data inside the functor object
+// and allow to execute operations like map and mutate the data in a single dotline
 const RightFunctor = value => ({
 	map: fn => RightFunctor(fn(value)),
 	log: () => {
@@ -10,6 +12,10 @@ const RightFunctor = value => ({
 	chain: fn => fn(value),
 });
 
+// When At some point the data is sended to a left functor because of an error or a
+// different is required, the left functor will skip the mutation of the data in a dotline flow
+// and usually redirect the message "stored in value", to a final callback in the hold section
+// and return something different usually an error
 const LeftFunctor = value => ({
 	map: fn => LeftFunctor(value),
 	log: () => {
@@ -20,8 +26,12 @@ const LeftFunctor = value => ({
 	chain: fn => LeftFunctor(value),
 });
 
+// When the data passed here is falsy, a LeftFunctor is called which will skip all functions
+// bellow a dotchain and will provide an error message (or null value) to the result of the flow
 const fromNullable = x => (x != null ? RightFunctor(x) : LeftFunctor(null));
 
+// When an erro happens Left functor will skip all the dotchain flow and send an error message
+// But if everything is correct the right functor will wrap the data and allow to mutate the data in the way we want
 const tryCatch = fn => {
 	try {
 		return RightFunctor(fn);
@@ -32,6 +42,7 @@ const tryCatch = fn => {
 
 const config = '{"url": "postgres://sally:muppets@localhost:5432/mydb"}';
 
+// Old way
 const startApp_ = config => {
 	const parsed = parseDbUrl(config);
 
@@ -41,12 +52,13 @@ const startApp_ = config => {
 	} else return "can't get config";
 };
 
+// New way
 const startApp = config =>
-	RightFunctor(config)
+	RightFunctor(config) // Creates the functor
 		.map(data => parseDbUrl(data))
-		.chain(data => fromNullable(data))
+		.chain(data => fromNullable(data)) // If the data is falsy return an error in the fold
 		.map(([_, user, password, db]) => `starting ${db}, ${user}, ${password}`)
 		.fold(
-			error => error,
-			data => data,
+			error => error,// the first callback will be executed
+			data => data,// But the data remains in the RightFunctor the second callback will be executed,
 		);
