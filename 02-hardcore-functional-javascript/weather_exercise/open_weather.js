d737f187f01282f65f7047596e8567241672c756
MauFrontend <104452684+MauFrontend@users.noreply.github.com>
2022-05-06 18:05:26 -0500

    Open weather description

--- 02-hardcore-functional-javascript/weather_exercise/open_weather.js
+++ 02-hardcore-functional-javascript/weather_exercise/open_weather.js
@@ -2,9 +2,11 @@ import { compose, map } from "ramda"
 import { Task } from "./Types"
 import apiKey from "./apikey"
 
+// this creates the url of the function
 const makeWeatherUrl = ({ zip, apiKey }) =>
   `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&appid=${apiKey}`
 
+// A fetch implementation that returns a Functor
 const fetchIt = url =>
   Task((reject, response) =>
     fetch(url)
@@ -16,7 +18,7 @@ const fetchIt = url =>
 const toJson = x => x.json()
 
 const OpenWeather = {
-  fetch: compose(fetchIt, makeWeatherUrl),
+  fetch: compose(fetchIt, makeWeatherUrl), // this receives the data zip and api key then pass the info to the fetch function and return the functor
 }
 
 export default OpenWeather
