373e1848f9c978621f92dc425e5a1c1d76001935
MauFrontend <104452684+MauFrontend@users.noreply.github.com>
2022-05-06 18:03:52 -0500

    Description of functions in the app

--- 02-hardcore-functional-javascript/weather_exercise/index.js
+++ 02-hardcore-functional-javascript/weather_exercise/index.js
@@ -6,30 +6,32 @@ const Weather = (dt, temp) => ({ dt, temp })
 
 const toFarenheit = k => k + 1000
 
+// this formats the data
 const toWeather = (dt, temp) =>
   Weather(new Date(dt / 1000).toLocaleDateString(), toFarenheit(temp))
 
+// PRivate function used in a map function and formats the data using toWeather function
 const prepareItems = w => toWeather(w.dt, w.main.temp)
 
 const getWeatherItems = zip =>
   OpenWeather.fetch({ zip, apiKey })
     .map(json => json.list.map(prepareItems))
-    .map(x => (console.log(x), x))
-    .map(weather => weather.map(toLi))
+    .map(x => (console.log(x), x))// logs the data and continues the flow
+    .map(weather => weather.map(toLi)) // Converts each element of the data in a string
 
 const toLi = weather => `<div>${weather.dt} ${weather.temp}</div>`
 
 // ======================================
 
 const app = () => {
-  const goButton = document.getElementById("go")
-  const input = document.getElementById("zip")
-  const results = document.getElementById("results")
+  const goButton = document.getElementById("go") // this button when clicked will start the call to the api
+  const input = document.getElementById("zip") // this will be an input, but we only use its data-set
+  const results = document.getElementById("results") // here we will put the results of the fetch
 
   goButton.addEventListener("click", () => {
-    const zipCode = input.value.trim()
+    const zipCode = input.value.trim() // get the ziPcode from the input
     getWeatherItems(zipCode).fork(console.error, html => {
-      results.innerHTML = html
+      results.innerHTML = html // here we put the data
     })
   })
 }
